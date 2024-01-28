98890f3caac0aa6651776d65683e586931c0a133
first-second <firstsecondis72@gmail.com>
2024-01-28 23:07:06 +0530

    touch up

--- news/app.py
+++ news/app.py
@@ -1,18 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 import requests
 from decouple import config
 
 app = Flask(__name__)
 app.static_url_path = '/static'
 app.static_folder = 'static'
+
+
 @app.route('/')
 def home():
-    API_KEY=config('API_KEY')
-    url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey='+API_KEY
+    api_key = config('API_KEY')
+    url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey='+api_key
     response = requests.get(url)
     data = response.json()
     articles = data['articles']
     return render_template("home.html", context=articles)
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000,)
